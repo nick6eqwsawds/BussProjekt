dfe8053a522c24669026bc7ab10c1fd360c77126
otto hoffman <otto.hoffman@outlook.com>
2024-04-15 09:35:38 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -6,37 +6,12 @@ public class App {
     static double vuxen_plats = 299.90;
     static double barn_plats = 149.90;
     static double vinst = 0;
+    //static String[] pasagerare = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
     static int[] pasagerare = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     public static void main(String[] args) throws Exception {
         //platser();
         meny();
 
-    }
-
-    static void platser() {
-
-        //pasagerare[0]= 0;
-
-        /* 
-        for (int i = 0; i < pasagerare.length; i++) {
-            System.out.println("|"+pasagerare[i]+" |");
-            
-
-        }*/
-
-        System.out.println("|1 |"+"|2 | "+" |3 |"+"|4 |");
-        System.out.println("|5 |"+"|6 | "+" |7 |"+"|8 |");
-        System.out.println("|9 |"+"|10| "+" |11|"+"|12|");
-        System.out.println("|13|"+"|14| "+" |15|"+"|16|");
-        System.out.println("|17|"+"|18| "+" |19|"+"|20|");
-
-        /* 
-        System.out.println("|"+pasagerare[0]+" |"+"|"+pasagerare[1]+" | "+" |"+pasagerare[2]+" |"+"|"+pasagerare[3]+" |");
-        System.out.println("|"+pasagerare[4]+" |"+"|"+pasagerare[5]+" | "+" |"+pasagerare[6]+" |"+"|"+pasagerare[7]+" |");
-        System.out.println("|"+pasagerare[8]+" |"+"|"+pasagerare[9]+"| "+" |"+pasagerare[10]+"|"+"|"+pasagerare[11]+"|");
-        System.out.println("|"+pasagerare[12]+"|"+"|"+pasagerare[13]+"| "+" |"+pasagerare[14]+"|"+"|"+pasagerare[15]+"|");
-        System.out.println("|"+pasagerare[16]+"|"+"|"+pasagerare[17]+"| "+" |"+pasagerare[18]+"|"+"|"+pasagerare[19]+"|");
-        */
     }
 
     public static void meny() throws Exception {
@@ -58,6 +33,7 @@ public class App {
             break;
             case 2:
             platser();
+            meny();
             break;
             case 3:
             //
@@ -66,6 +42,37 @@ public class App {
             break;
         }
 
+    }
+
+    static void platser() throws Exception{
+
+        //pasagerare[0]= 0;
+
+        
+        for (int i = 0; i < pasagerare.length; i++) {
+            if(pasagerare[i]<=20){
+                System.out.println(pasagerare[i]+" är ledig");
+            }
+            //System.out.println("|"+pasagerare[i]+" |");
+            
+
+        }
+
+        System.out.println("|1 |"+"|2 | "+" |3 |"+"|4 |");
+        System.out.println("|5 |"+"|6 | "+" |7 |"+"|8 |");
+        System.out.println("|9 |"+"|10| "+" |11|"+"|12|");
+        System.out.println("|13|"+"|14| "+" |15|"+"|16|");
+        System.out.println("|17|"+"|18| "+" |19|"+"|20|");
+
+        /* 
+        System.out.println("|"+pasagerare[0]+" |"+"|"+pasagerare[1]+" | "+" |"+pasagerare[2]+" |"+"|"+pasagerare[3]+" |");
+        System.out.println("|"+pasagerare[4]+" |"+"|"+pasagerare[5]+" | "+" |"+pasagerare[6]+" |"+"|"+pasagerare[7]+" |");
+        System.out.println("|"+pasagerare[8]+" |"+"|"+pasagerare[9]+"| "+" |"+pasagerare[10]+"|"+"|"+pasagerare[11]+"|");
+        System.out.println("|"+pasagerare[12]+"|"+"|"+pasagerare[13]+"| "+" |"+pasagerare[14]+"|"+"|"+pasagerare[15]+"|");
+        System.out.println("|"+pasagerare[16]+"|"+"|"+pasagerare[17]+"| "+" |"+pasagerare[18]+"|"+"|"+pasagerare[19]+"|");
+        */
+        
+        
     }
 
     public static void Bokning() throws Exception {
@@ -105,6 +112,7 @@ public class App {
             System.out.println("Dessa plattser är lediga:");
             Thread.sleep(750);
             platser();
+            /* 
             pasagerare[0]=0;
             pasagerare[1]=0;
             pasagerare[2]=0;
@@ -125,6 +133,7 @@ public class App {
             pasagerare[17]=0;
             pasagerare[18]=0;
             pasagerare[19]=0;
+            */
             Thread.sleep(500);
             System.out.println("Vilken platts vill du boka?");
             int plats_bokning = tangentbord.nextInt();
